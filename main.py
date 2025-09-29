a75b248faa45e1c34aefdf3ff50072ade186aada
PairZhu <1115306638@qq.com>
2025-09-29 16:50:00 +0800

    feat: 自动重连

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ logger = logging.getLogger(__name__)
 dotenv.load_dotenv()
 
 
-async def main():
+async def qq_bot_server():
     async with websockets.connect(
         uri=os.getenv("ROBOT_WS_URL"),
         additional_headers={"Authorization": f"Bearer {os.getenv('QQ_TOKEN')}"},
     ) as ws:
-
         def send_private_msg(user_id: int, msg: str):
             asyncio.create_task(
                 ws.send(
@@ -54,21 +53,36 @@ async def main():
 
         robot = ChargeRobot(charge_listener, send_private_msg)
         logger.info("ChargeRobot started")
+        send_private_msg(
+            int(os.getenv("MASTER_QQ")),
+            "ChargeRobot已启动！\n输入 'charge help' 获取帮助信息。",
+        )
 
-        try:
-            async for ws_message in ws:
-                message_data = json.loads(ws_message)
-                if message_data.get("message_type") == "private":
-                    user_id: int = message_data["user_id"]
-                    message: str = message_data["raw_message"]
-                    robot.handle_message(user_id, message)
-        except Exception as e:
-            logger.error(f"Main loop error: {e}")
+        async for ws_message in ws:
+            message_data = json.loads(ws_message)
+            if message_data.get("message_type") == "private":
+                user_id: int = message_data["user_id"]
+                message: str = message_data["raw_message"]
+                robot.handle_message(user_id, message)
 
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
-    asyncio.run(main())
+
+    async def loop():
+        while True:
+            try:
+                await qq_bot_server()
+            except Exception as e:
+                logger.error(f"qq_bot_server error: {e}")
+            logger.info("Reconnecting to qq_bot_server in 15 seconds...")
+            await asyncio.sleep(15)
+
+    asyncio.run(loop())
+
+
+if __name__ == "__main__":
+    main()
